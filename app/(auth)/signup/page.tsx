7e9b037c6f92d78ffd253c60ec7c689af50fe1b2
Ahmed Hussain <ahmedhussainnow@gmail.com>
2025-12-11 21:33:25 +0600

    partial signup pg designing

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,204 @@
+"use client";
+
+import Logo from "@/components/logo";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import * as z from "zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Loader } from "lucide-react";
+
+function SignUp() {
+  const formSchema = z
+    .object({
+      name: z.string().trim().min(1, {
+        message: "Name is required",
+      }),
+      email: z.string().trim().email().min(1, {
+        message: "Email is required",
+      }),
+      password: z.string().trim().min(1, {
+        message: "Password is required",
+      }),
+      confirmPassword: z.string().min(1, {
+        message: "Confirm Password is required",
+      }),
+    })
+    .refine((val) => val.password === val.confirmPassword, {
+      message: "Password does not match",
+      path: ["confirmPassword"],
+    });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  return (
+    <>
+      <main className="w-full min-h-[500px] h-auto max-w-full pt-10">
+        {
+          //isSubmit?
+
+          <div className="w-full p-5 rounded-md">
+            <Logo />
+            <h1 className="text-xl tracking-[-0.16px] dark:text-[#fcfdffef] font-bold mb-1.5 mt-8 text-center sm:text-left">
+              Create an Account{" "}
+            </h1>
+            <p className="mb-6 text-center sm:text-left text-base dark:text-[#f1f7feb5] font-normal">
+              Already have an account?
+              <Link className="text-primary" href="/">
+                Sign in
+              </Link>
+            </p>
+            <Form {...form}>
+              <form>
+                <div className="mb-4">
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                          Name
+                        </FormLabel>
+                        <FormControl>
+                          <Input placeholder="type your name" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                          Email
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="your@mail.com"
+                            autoComplete="off"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="mb-4">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                          Password
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="••••••••••••"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <FormField
+                    control={form.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                          Confirm Password
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="••••••••••••"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <Button
+                  className="w-full text-[15px] h-[40px] bg-blue-500 text-white font-semibold"
+                  //   disabled={isPending}
+                  type="submit"
+                >
+                  {/* {isPending && <Loader className="animate-spin" />} */}
+                  Create account
+                  <ArrowRight />
+                </Button>
+
+                <div className="mb-4 mt-4 flex items-center justify-center">
+                  <div
+                    aria-hidden="true"
+                    className="h-px w-full bg-[#eee] dark:bg-[#d6ebfd30]"
+                    data-orientation="horizontal"
+                    role="separator"
+                  ></div>
+                  <span className="mx-4 text-xs dark:text-[#f1f7feb5] font-normal">
+                    OR
+                  </span>
+                  <div
+                    aria-hidden="true"
+                    className="h-px w-full bg-[#eee] dark:bg-[#d6ebfd30]"
+                    data-orientation="horizontal"
+                    role="separator"
+                  ></div>
+                </div>
+              </form>
+            </Form>
+            <Button variant="outline" className="w-full h-[40px]">
+              Email magic link
+            </Button>
+            <p className="text-xs font-normal mt-4">
+              By signing up, you agree to our{" "}
+              <a className="text-primary hover:underline" href="#">
+                Terms of Service
+              </a>{" "}
+              and{" "}
+              <a className="text-primary hover:underline" href="#">
+                Privacy Policy
+              </a>
+              .
+            </p>
+          </div>
+        //   : (<div> </div>)
+        }
+      </main>
+    </>
+  );
+}
+
+export default SignUp;
