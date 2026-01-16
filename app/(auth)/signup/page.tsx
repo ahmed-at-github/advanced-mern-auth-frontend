e7ea9da95765309970ecb0bff3a40e73128cc69a
Ahmed Hussain <ahmedhussainnow@gmail.com>
2026-01-16 10:29:43 +0600

    style: add skeleton for signup page

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -16,7 +16,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { ArrowRight, Loader } from "lucide-react";
+import { ArrowRight, Loader, MailCheckIcon } from "lucide-react";
 
 function SignUp() {
   const formSchema = z
@@ -54,147 +54,167 @@ function SignUp() {
       <main className="w-full min-h-[500px] h-auto max-w-full pt-10">
         {
           //isSubmit?
+          false ? (
+            <div className="w-full p-5 rounded-md">
+              <Logo />
+              <h1 className="text-xl tracking-[-0.16px] dark:text-[#fcfdffef] font-bold mb-1.5 mt-8 text-center sm:text-left">
+                Create an Account{" "}
+              </h1>
+              <p className="mb-6 text-center sm:text-left text-base dark:text-[#f1f7feb5] font-normal">
+                Already have an account?
+                <Link className="text-primary" href="/">
+                  {" "}
+                  Sign in
+                </Link>
+              </p>
+              <Form {...form}>
+                <form>
+                  <div className="mb-4">
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                            Name
+                          </FormLabel>
+                          <FormControl>
+                            <Input placeholder="type your name" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
 
-          <div className="w-full p-5 rounded-md">
-            <Logo />
-            <h1 className="text-xl tracking-[-0.16px] dark:text-[#fcfdffef] font-bold mb-1.5 mt-8 text-center sm:text-left">
-              Create an Account{" "}
-            </h1>
-            <p className="mb-6 text-center sm:text-left text-base dark:text-[#f1f7feb5] font-normal">
-              Already have an account?
-              <Link className="text-primary" href="/">
-                Sign in
-              </Link>
-            </p>
-            <Form {...form}>
-              <form>
-                <div className="mb-4">
-                  <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel className="dark:text-[#f1f7feb5] text-sm">
-                          Name
-                        </FormLabel>
-                        <FormControl>
-                          <Input placeholder="type your name" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
+                  <div className="mb-4">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                            Email
+                          </FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="your@mail.com"
+                              autoComplete="off"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                            Password
+                          </FormLabel>
+                          <FormControl>
+                            <Input
+                              type="password"
+                              placeholder="••••••••••••"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
 
-                <div className="mb-4">
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel className="dark:text-[#f1f7feb5] text-sm">
-                          Email
-                        </FormLabel>
-                        <FormControl>
-                          <Input
-                            placeholder="your@mail.com"
-                            autoComplete="off"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <div className="mb-4">
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel className="dark:text-[#f1f7feb5] text-sm">
-                          Password
-                        </FormLabel>
-                        <FormControl>
-                          <Input
-                            type="password"
-                            placeholder="••••••••••••"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
+                  <div className="mb-4">
+                    <FormField
+                      control={form.control}
+                      name="confirmPassword"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                            Confirm Password
+                          </FormLabel>
+                          <FormControl>
+                            <Input
+                              type="password"
+                              placeholder="••••••••••••"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <Button
+                    className="w-full text-[15px] h-[40px] bg-blue-500 text-white font-semibold"
+                    //   disabled={isPending}
+                    type="submit"
+                  >
+                    {/* {isPending && <Loader className="animate-spin" />} */}
+                    Create account
+                    <ArrowRight />
+                  </Button>
 
-                <div className="mb-4">
-                  <FormField
-                    control={form.control}
-                    name="confirmPassword"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel className="dark:text-[#f1f7feb5] text-sm">
-                          Confirm Password
-                        </FormLabel>
-                        <FormControl>
-                          <Input
-                            type="password"
-                            placeholder="••••••••••••"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <Button
-                  className="w-full text-[15px] h-[40px] bg-blue-500 text-white font-semibold"
-                  //   disabled={isPending}
-                  type="submit"
-                >
-                  {/* {isPending && <Loader className="animate-spin" />} */}
-                  Create account
-                  <ArrowRight />
+                  <div className="mb-4 mt-4 flex items-center justify-center">
+                    <div
+                      aria-hidden="true"
+                      className="h-px w-full bg-[#eee] dark:bg-[#d6ebfd30]"
+                      data-orientation="horizontal"
+                      role="separator"
+                    ></div>
+                    <span className="mx-4 text-xs dark:text-[#f1f7feb5] font-normal">
+                      OR
+                    </span>
+                    <div
+                      aria-hidden="true"
+                      className="h-px w-full bg-[#eee] dark:bg-[#d6ebfd30]"
+                      data-orientation="horizontal"
+                      role="separator"
+                    ></div>
+                  </div>
+                </form>
+              </Form>
+              <Button variant="outline" className="w-full h-[40px]">
+                Email magic link
+              </Button>
+              <p className="text-xs font-normal mt-4">
+                By signing up, you agree to our{" "}
+                <a className="text-primary hover:underline" href="#">
+                  Terms of Service
+                </a>{" "}
+                and{" "}
+                <a className="text-primary hover:underline" href="#">
+                  Privacy Policy
+                </a>
+                .
+              </p>
+            </div>
+          ) : (
+            <div className="w-full h-[80vh] flex flex-col gap-2 items-center justify-center rounded-md">
+              <div className="size-[48px]">
+                <MailCheckIcon size="48px" className="animate-bounce" />
+              </div>
+              <h2 className="text-xl tracking-[-0.16px] dark:text-[#fcfdffef] font-bold">
+                {" "}
+                Check your email
+              </h2>
+              <p className="mb-2 text-center text-sm text-muted-foreground dark:text-[#f1f7feb5] font-normal">
+                {" "}
+                We just sent a verfication link to {form.getValues().email}
+              </p>
+              <Link href={"/"}>
+                <Button className="h-[40px]">
+                  Go to login <ArrowRight />
                 </Button>
-
-                <div className="mb-4 mt-4 flex items-center justify-center">
-                  <div
-                    aria-hidden="true"
-                    className="h-px w-full bg-[#eee] dark:bg-[#d6ebfd30]"
-                    data-orientation="horizontal"
-                    role="separator"
-                  ></div>
-                  <span className="mx-4 text-xs dark:text-[#f1f7feb5] font-normal">
-                    OR
-                  </span>
-                  <div
-                    aria-hidden="true"
-                    className="h-px w-full bg-[#eee] dark:bg-[#d6ebfd30]"
-                    data-orientation="horizontal"
-                    role="separator"
-                  ></div>
-                </div>
-              </form>
-            </Form>
-            <Button variant="outline" className="w-full h-[40px]">
-              Email magic link
-            </Button>
-            <p className="text-xs font-normal mt-4">
-              By signing up, you agree to our{" "}
-              <a className="text-primary hover:underline" href="#">
-                Terms of Service
-              </a>{" "}
-              and{" "}
-              <a className="text-primary hover:underline" href="#">
-                Privacy Policy
-              </a>
-              .
-            </p>
-          </div>
-        //   : (<div> </div>)
+              </Link>
+            </div>
+          )
         }
       </main>
     </>
