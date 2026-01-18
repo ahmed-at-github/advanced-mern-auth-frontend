63c4dac15f767e703d85821a8369f04cc367beb4
Ahmed Hussain <ahmedhussainnow@gmail.com>
2026-01-18 18:31:29 +0600

    feat: add(ed) EnableMfa component
    toast is not implemented at ln 73-89

--- lib/api.ts
+++ lib/api.ts
@@ -17,6 +17,14 @@ type forgotPasswordType = { email: string };
 type resetPasswordType = { password: string; verificationCode: string };
 type verifyEmailType = { code: string };
 
+export type mfaType = {
+    message: string;
+    secret: string;
+    qrImageUrl: string;
+};
+
+type verifyMFAType = { code: string; secretKey: string };
+
 
 export const loginMutationFn = async (data: LoginType) => {
     await API.post(`/auth/login`, data)
@@ -36,3 +44,11 @@ export const verifyEmailMutationFn = async (data: verifyEmailType) =>
     await API.post(`/auth/verify/email`, data);
 
 export const getUserSessionQueryFn = async () => await API.get(`/session/`);
+
+export const mfaSetupQueryFn = async () => {
+    const response = await API.get<mfaType>(`/mfa/setup`);
+    return response.data;
+};
+
+export const verifyMFAMutationFn = async (data: verifyMFAType) =>
+  await API.post(`/mfa/verify`, data);
